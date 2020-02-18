f56700b9ee7b2a6bacf6298836005a06a42d82ef
mgkyawzayya <kyawzayya.ucsp@gmail.com>
2020-02-18 13:30:17 +0630

    add parcel build process

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-const Pet = ({
-  name,
-  animal,
-  breed
-}) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h3', {}, breed)
-  ]);
-};
+import React from 'react';
+import { render } from "react-dom";
+import Pet from './Pet';
 
 const App = () => {
   return React.createElement(
@@ -16,7 +8,7 @@ const App = () => {
       id: 'container'
     },
     [
-      React.createElement('h1', {}, 'Adopt Me!'),
+      React.createElement('h1', {}, 'Adopt Me!'), 
       React.createElement(Pet, {
         name: 'Luna',
         animal: 'Dog',
@@ -35,4 +27,4 @@ const App = () => {
     ]
   );
 };
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+render(React.createElement(App), document.getElementById('root'));
